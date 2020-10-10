0bbdfd793f25f84fa14e3138f187515ab9d3c72b
Arnav Das <50672340+arnav257@users.noreply.github.com>
2020-10-10 16:35:48 +0530

    update test.py

--- ExoplanetPy/test.py
+++ ExoplanetPy/test.py
@@ -2,6 +2,7 @@ from planet import Planet
 from system import System
 
 
-p1 = Planet(e=0.0, a=4, omega=0, Omega=0, i=89.9, r_p=0.1, first_periastron=0.0)
-sys = System(star_prop={'Mass': 4}, planet_list=[p1], sort=True)
+p1 = Planet(e=0.0, a=8, omega=0, Omega=0, i=89.9, r_p=0.1, first_periastron=0.0)
+p2 = Planet(e=0.0, a=2, omega=0, Omega=0, i=89.9, r_p=0.05, first_periastron=0.0)
+sys = System(star_prop={'Mass': 4}, planet_list=[p1,p2], sort=True)
 sys.plot(model='Quadratic', normalise=True)
