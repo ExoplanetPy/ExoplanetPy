c1247c3e79a14da65206a3d30b6b58fceece90d3
Arnav Das <50672340+arnav257@users.noreply.github.com>
2020-10-10 17:10:23 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ExoplanetPy',
+      version='1.0',
+      description='Package for exoplanet modelling',
+      license='MIT',
+      long_description='A python package for modelling the transit light curves of systems with multiple exoplanets around their host stars.',
+      author='A. Das, D. Jain',
+      author_email='arnav257@gmail.com',
+      url='https://github.com/ExoplanetPy/ExoplanetPy',
+      packages=['ExoplanetPy'],
+     )
