3d25a38229c891cb4d7b14b9c5f7f0d6b1d31abe
devansh-dvj <devansh.dvj@gmail.com>
2020-10-20 02:24:37 +0530

    Updated the test system

--- ExoplanetPy/test_v0.py
+++ ExoplanetPy/test_v0.py
@@ -1,14 +1,11 @@
 from planet import Planet
 from system import System
 
-p1 = Planet(e=0.0, a=8, omega=0, Omega=0, i=89.9, r_p=0.1, first_periastron=0.0)
-p2 = Planet(e=0.0, a=2, omega=0, Omega=0, i=89.9, r_p=0.05, first_periastron=0.0)
-sys = System(star_prop={'Mass': 4}, planet_list=[p1, p2], sort=True)
+p1 = Planet(e=0.0, a=8, omega=0, Omega=0, i=89, r_p=0.1, first_periastron=0.03)
+p2 = Planet(e=0.0, a=4, omega=0, Omega=0, i=89, r_p=0.07, first_periastron=0.52)
+p3 = Planet(e=0.0, a=2, omega=0, Omega=0, i=87, r_p=0.05, first_periastron=0.0)
+sys = System(star_prop={'Mass': 4}, planet_list=[p1, p3, p2], sort=True)
 sys.plot(model='Quadratic', normalise=True)
-sys.visualize(time=0.02, model='Quadratic')
-sys.visualize(time=0.03, model='Quadratic')
-sys.visualize(time=0.04, model='Quadratic')
-sys.visualize(time=0.05, model='Quadratic')
 sys.visualize(time=0.26, model='Quadratic')
 sys.visualize(time=0.27, model='Quadratic')
 sys.visualize(time=0.28, model='Quadratic')
